dc9f236354868e7cbe34985ab9bba26b51f84633
renanbortoletto <85020822+renanbortoletto@users.noreply.github.com>
2021-07-21 13:41:30 -0300

    Foi criado removerContato e Screenshot.

--- src/test/java/tests/InformacoesUsuarioTes.java
+++ src/test/java/tests/InformacoesUsuarioTes.java
@@ -1,21 +1,125 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import suporte.Generator;
+import suporte.Screenshot;
+
+import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTes {
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+    private WebDriver navegador;
+
+    @Rule
+    public TestName test = new TestName();
+
+    @Before
+    public void setUp(){
         //Abrindo o navegador
         System.setProperty("webdriver.chrome.driver", "/Users/renanbortoletto/drivers/chromedriver");
-        WebDriver navegador = new ChromeDriver();
+        navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //Navegando para a pagina do Taskit
-        navegador.get("http://www.renanbortoletto.com.br/taskit");
+        navegador.get("http://www.juliodelima.com.br/taskit");
 
-        //Validaçao
-        assertEquals(1,1);
+
+        //Clicar no link que possui o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
+
+
+        //Identificando o formulario de Login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
+
+        //Digitar no campo com nome "Login"que esta dentro do formulario de ID "Signinbox" o texto "renanbortoletto"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("renanbortoletto");
+
+
+        //Digitar no campo com nome "password"que esta dentro do formulario de ID "Signinbox" o texto "21109400re"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("21109400re");
+
+        //Clicar no link com o texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
+
+        //Clicar em um link que possui a class "me"
+        navegador.findElement(By.className("me")).click();
+
+        //Clicar em um link que possui o texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+    }
+
+
+    //@Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+
+        //Clicar no botao atraves do seu xpath "//button[@data-target="addmoredata"]"
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        //Identificar o popUp onde esta o formulario de id "addmoredata"
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+
+        //Na combo de "Name Type" escolher a opcao "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+        new Select(campoType).selectByVisibleText("Phone");
+
+        //No campo de name "contact" digitar "+5511987654321"
+        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5511987654321");
+
+        //Clicar no link de text "SAVE" que esta no popUp
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        //Na mensagem de id "toast-container" validar que o texto e "Your contact has been added!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Your contact has been added!", mensagem);
+
+    }
+
+    @Test
+    public void removerUmContatoDeUmUsuario(){
+        // 11878767675
+        //Clicar no elemento pelo seu xpath "secondary-content button"
+        navegador.findElement(By.xpath("//span[text()=\"11878767675\"]/following-sibling::a")).click();
+
+        // Clicar no botao OK
+        navegador.switchTo().alert().accept();
+
+        // validar a mensagem "Rest in peace, dear phone!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Rest in peace, dear phone!", mensagem);
+
+        String screenshotArquivo = "/Users/renanbortoletto/" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
+        Screenshot.tirar(navegador, screenshotArquivo);
+
+        //Aguardar ate 10seg para que a janela desapareca
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
+
+        //Fazer Logout
+        navegador.findElement(By.linkText("Logout")).click();
+
+    }
+
+    @After
+    public void tearDown(){
+        //Fechar o navegador
+        navegador.quit();
+
     }
 }
